73a3419a33b8005970f4a0e4bebe9c51ce4db993
Kolima62 <163014133+Kolima62@users.noreply.github.com>
2024-04-02 16:37:51 +0200

    Add files via upload

---
+++ boucle.js
@@ -0,0 +1,12 @@
+const tab = [
+  [1, 7],
+  ["test", "tata"],
+];
+
+// boucler et afficher 1, 7, test et tata
+
+for (let i = 0; i < tab.length; i++) {
+  for (let u = 0; u < tab[i].length; u++) {
+    console.log(tab[i][u]);
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const age = "18";
+// function ecrireUnMot() {
+//   // instruction
+//   console.log("un mot");
+// }
 
-if (age !== 18) {
-  console.log("truc");
-} else if (age === 18) {
-  console.log("pas truc");
+// ecrireUnMot(); // 'un mot'
+
+// function nombreEstPair(nbr) {
+//   const ispair = nbr % 2 === 0 ? "pair" : "impair";
+//   const str = `Le nombre ${nbr} est ${ispair}`;
+//   console.log(str);
+// }
+
+// fonction fléchée
+// const nombreEstPair = (nbr) => {
+//   const ispair = nbr % 2 === 0 ? "pair" : "impair";
+//   const str = `Le nombre ${nbr} est ${ispair}`;
+//   console.log(str);
+// };
+
+// const function = nombreEstPair(nbr) {
+//   const ispair = nbr % 2 === 0 ? "pair" : "impair";
+//   const str = `Le nombre ${nbr} est ${ispair}`;
+//   console.log(str);
+// }
+
+// nombreEstPair(44);
+// nombreEstPair(3);
+
+// const addition = (a, b) => console.log(a + b);
+
+// function test(nbr1, nbr2) {
+//   if (nbr1 > 10) {
+//     addition(nbr1, nbr2); // appel fonction
+//   }
+// }
+
+// test(11, 10);
+
+// const test = () => console.log("Vous êtes majeur.");
+// const test2 = () => console.log("Vous êtes majeur.");
+
+function isAdulte(age, cb) {
+  if (age > 18) {
+    console.log("majeur");
+  }
+  cb();
 }
+
+isAdulte(19, () => console.log("coucou"));
+isAdulte(19, function () {
+  console.log("coucou");
+});
